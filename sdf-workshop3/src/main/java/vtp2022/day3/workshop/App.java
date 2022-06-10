eb0a6c75d4a7a9201f15dc3fdc5ed4cf066a7eba
Hong <hyew.lim@gmail.com>
2022-06-10 09:01:59 +0800

    yes sir

--- sdf-workshop3/src/main/java/vtp2022/day3/workshop/App.java
+++ sdf-workshop3/src/main/java/vtp2022/day3/workshop/App.java
@@ -1,5 +1,7 @@
 package vtp2022.day3.workshop;
 
+
+
 /**
  * Main program
  *
@@ -9,13 +11,18 @@ public class App
     private static String defaultDb= "db";
     public static void main( String[] args )
     {
+        
         // print out first argument db name use to create the directory
         if(args.length > 0)
             if(args[0] != null){
                 System.out.println( args[0] );
                 App.defaultDb = args[0];
             }
+        
         System.out.println( defaultDb );
+        Repository repo = new Repository(defaultDb);
+        Session session = new Session(repo); //session controls the inpuut of command
+        session.start();
 
     }
 }
